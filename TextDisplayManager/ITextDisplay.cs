2f7bfeb89c8100c0ade62c0f3533c4323803fecf
Ben Thomas <bethoma@microsoft.com>
2015-09-11 16:49:29 -0700

    1.0.0.0 Release
    Notes:
    Screen config support
    HD44780 chip support in 4-Bit mode ONLY

---
+++ TextDisplayManager/ITextDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Windows.Foundation;
+
+namespace TextDisplay
+{
+    public interface ITextDisplay
+    {
+        uint Height
+        {
+            get;
+        }
+
+        uint Width
+        {
+            get;
+        }
+
+        IAsyncAction InitializeAsync();
+
+        IAsyncAction DisposeAsync();
+
+        IAsyncAction WriteMessageAsync(string message, uint timeout);
+    }
+}
